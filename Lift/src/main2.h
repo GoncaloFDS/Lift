3b195f52da113bf927348a94d417c938ec5ae351
Gonçalo Soares <goncalofds@gmail.com>
2019-09-01 22:17:10 +0100

    rearranged project structure

---
+++ Lift/src/main2.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Application.h"
+
+#ifdef LF_PLATFORM_WINDOWS
+
+int main(int argc, char* argv[]) {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	lift::Log::Init();
+	LF_CORE_INFO("Initialized Log!");
+
+	std::shared_ptr<lift::Application> app = lift::CreateApplication();
+
+	app->Run();
+
+	return 0;
+}
+
+#endif
